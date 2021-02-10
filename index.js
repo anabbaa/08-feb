62edf944aee01075e28e8970ae272233a2cb9df6
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-02-10 21:45:41 +0100

    second

--- index.js
+++ index.js
@@ -45,23 +45,6 @@ const crazy = (str)=>{
 };
 console.log(crazy("javascript"));
 
-// Tell me more
-// Create a function that accepts the (user name, age, address and unlimited number of activities the user enjoy doing)
-// print all the user input adding the greeting part and more text turning that input into a readable text.
-// If one of his activities was dance or party add "you are cool" to your outputted text.
-// Examples :
-// userData("Zain", 22, "Kurfürstendamm", "dance", "party");
-// userData("Olga", 40, "potsdamer platz", "party");
-// userData("Nancy", 33, "viktoria luise platz", "swimming");
-const tellmemore = (username,age,add,...activities)=>{
-    let result = `your name is ${username} your age is ${age} you live in ${add} 
-    you make fun with ${activities.join(",")}`;
 
-    if (activities == ("dance") || activities == ("party") ){
-        result +=  "you are nice";
-    } 
-    return result;
-};
-console.log(tellmemore("Zain", 22, "Kurfürstendamm", "party"));
 
 
